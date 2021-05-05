8411b22a1b25994d7ef94c6a913f28336cbcee01
bopporgo <gopi.srcm@gmail.com>
2021-05-05 18:20:29 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file("gopiproject2021-1a26a578350e.json")
+
+  project = "gopiproject2021"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email = "gopiprojserviceaccount@gopiproject2021.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+}
